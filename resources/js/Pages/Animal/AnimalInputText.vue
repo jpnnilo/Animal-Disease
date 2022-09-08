396e2c01f304f23f9eccc6d23883957b146449d6
jpnnilo <53546940+jpnnilo@users.noreply.github.com>
2022-09-08 13:49:40 +0800

    fixed animalinputtext

--- resources/js/Pages/Animal/AnimalInputText.vue
+++ resources/js/Pages/Animal/AnimalInputText.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="my-3">
         <label for="email" class="mb-3 block text-base font-medium text-[#07074D]" >
-            {{ title }}:  {{ modelValue }}
+            {{ title }}: 
         </label>
         <input type="text" :value="modelValue"  @input="emits('update:modelValue', $event.target.value)" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"/>
     </div>
